ad07f27c73fcbfb36bbfadb0132673b245698ef6
Dirk Faust <dirk@faustbande.de>
2017-12-29 09:47:03 +0100

    Button functionality

--- MetalBook/MetalBook/ViewController.swift
+++ MetalBook/MetalBook/ViewController.swift
@@ -11,19 +11,32 @@ import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
     @IBOutlet var webkitView: WKWebView!
 
-    func loadPage() {
-        let myURL = URL(string: "http://m.facebook.com/")
-        let myRequest = URLRequest(url: myURL!)
+    @IBAction func homeTapped(_ sender: Any)
+    {
+        loadPage(url: "https://www.facebook.com/?sk=h_nor")
+    }
+    
+    @IBAction func newestTapped(_ sender: Any) {
+        loadPage(url: "https://www.facebook.com/?sk=h_chr")
+    }
+    
+    @IBAction func messagesTapped(_ sender: Any) {
+        loadPage(url: "https://www.facebook.com/messages")
+    }
+    
+    func loadPage(url: String) {
+        let myRequest = URLRequest(url: URL(string: url)!)
         webkitView.stopLoading()
         webkitView.load(myRequest)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        loadPage()
+
+        loadPage(url: "https://www.facebook.com/")
     }
 
     override func didReceiveMemoryWarning() {
